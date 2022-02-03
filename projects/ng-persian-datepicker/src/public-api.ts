a083f168986520d61a501928f851919ee4ee88ff
SaeedPooyanfar <saeedp47@gmail.com>
2022-02-03 22:52:53 +0330

    remove unused service

--- projects/ng-persian-datepicker/src/public-api.ts
+++ projects/ng-persian-datepicker/src/public-api.ts
@@ -2,7 +2,6 @@
  * Public API Surface of ng-persian-datepicker
  */
 
-export * from './lib/ng-persian-datepicker.service';
 export * from './lib/ng-persian-datepicker.component';
 export * from './lib/ng-persian-datepicker.module';
 export * from './lib/interface/IConfig';

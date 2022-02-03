4889225ee128732394a117d4958d3462dfbe93ab
SaeedPooyanfar <saeedp47@gmail.com>
2022-02-03 22:51:11 +0330

    update demo

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -1,39 +1,26 @@
-import { IConfig } from '../../../projects/ng-persian-datepicker/src/lib/interface/IConfig';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-demo',
   templateUrl: './demo.component.html',
   styleUrls: ['./demo.component.scss']
 })
-export class DemoComponent implements OnInit {
+export class DemoComponent {
 
-  constructor(
-  ) {}
-
-  config: IConfig = {
-    date: {
-      value: new Date().valueOf(),
-      onSelect: (shamsiDate: string, gregorianDate: string, timestamp: number) => {
-        console.log(shamsiDate, gregorianDate, timestamp);
-      }
-    },
-    ui: {
-      theme: 'default',
-      isVisible: true,
-      hideAfterSelectDate: false,
-      hideOnOutsideClick: false,
-      yearView: true,
-      monthView: true
-    },
-    time: {
-      enable: true,
-      showSecond: true,
-      meridian: false
-    }
-  };
-
-  ngOnInit() {
+  dateValue = new Date().valueOf();
+  dateOnSelect = (shamsiDate: string, gregorianDate: string, timestamp: number) => {
+    console.log(shamsiDate, gregorianDate, timestamp);
   }
+
+  uiIsVisible: boolean = true;
+  uiTheme: string = 'default';
+  uiYearView: boolean = true;
+  uiMonthView: boolean = true;
+  uiHideAfterSelectDate: boolean = false;
+  uiHideOnOutsideClick: boolean = false;
+
+  timeEnable: boolean = true;
+  timeShowSecond: boolean = true;
+  timeMeridian: boolean = false;
 
 }

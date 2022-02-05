31a3d92e4779c61b0b957cf85ac2d820824a86bc
SaeedPooyanfar <saeedp47@gmail.com>
2022-02-05 19:56:58 +0330

    remove unused interfaces

--- projects/ng-persian-datepicker/src/lib/interface/_index.ts
+++ projects/ng-persian-datepicker/src/lib/interface/_index.ts
@@ -1,19 +1,11 @@
 import { IActiveDate } from './IActiveDate';
-import { IConfig } from './IConfig';
-import { IDate } from './IDate';
 import { IDay } from './IDay';
 import { IMonth } from './IMonth';
-import { ITime } from './ITime';
-import { IUi } from './IUi';
 import { IYear } from './IYear';
 
 export {
   IActiveDate,
-  IConfig,
-  IDate,
   IDay,
   IMonth,
-  ITime,
-  IUi,
   IYear
 };

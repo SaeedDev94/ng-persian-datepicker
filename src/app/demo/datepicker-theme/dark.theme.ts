69ab80815570b4d3d8baeea978975dd761f3dff3
SaeedPooyanfar <saeedp47@gmail.com>
2022-05-18 19:53:13 +0430

    update demo for custom theme feature

---
+++ src/app/demo/datepicker-theme/dark.theme.ts
@@ -0,0 +1,24 @@
+import { IDatepickerTheme } from '../../../../projects/ng-persian-datepicker/src/public-api';
+
+export const darkTheme: IDatepickerTheme = {
+  border: '#393939',
+  timeBorder: '#393939',
+
+  background: '#222222',
+  text: '#FFFFFF',
+
+  hoverBackground: '#393939',
+  hoverText: '#FFFFFF',
+
+  disabledBackground: '#393939',
+  disabledText: '#CCCCCC',
+
+  selectedBackground: '#D68E3A',
+  selectedText: '#FFFFFF',
+
+  todayBackground: '#FFFFFF',
+  todayText: '#2D2D2D',
+
+  otherMonthBackground: 'rgba(0, 0, 0, 0)',
+  otherMonthText: '#CCCCCC'
+};
--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -1,5 +1,10 @@
+import {
+  defaultTheme,
+  IActiveDate,
+  IDatepickerTheme
+} from '../../../projects/ng-persian-datepicker/src/public-api';
+import { darkTheme } from './datepicker-theme/dark.theme';
 import { Component } from '@angular/core';
-import { IActiveDate } from '../../../projects/ng-persian-datepicker/src/public-api';
 import { FormControl } from '@angular/forms';
 
 @Component({
@@ -12,7 +17,7 @@ export class DemoComponent {
   dateValue = new FormControl(new Date().valueOf());
 
   uiIsVisible: boolean = true;
-  uiTheme: string = 'default';
+  uiTheme: IDatepickerTheme = defaultTheme;
   uiYearView: boolean = true;
   uiMonthView: boolean = true;
   uiHideAfterSelectDate: boolean = false;
@@ -22,6 +27,25 @@ export class DemoComponent {
   timeEnable: boolean = true;
   timeShowSecond: boolean = true;
   timeMeridian: boolean = false;
+
+  private _theme: string = 'default';
+
+  get theme(): string {
+    return this._theme;
+  }
+
+  set theme(value: string) {
+    this._theme = value;
+
+    switch (value) {
+      case 'dark':
+        this.uiTheme = darkTheme;
+        break;
+      case 'default':
+        this.uiTheme = defaultTheme;
+        break;
+    }
+  }
 
   onSelect(date: IActiveDate) {
     console.log(date);

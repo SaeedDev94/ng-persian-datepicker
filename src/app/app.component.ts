88261debf3d9d1cc4a83138c9b6c2215962d30b6
SaeedDev94 <saeedp47@gmail.com>
2024-05-18 11:27:06 +0330

    standalone AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  standalone: true,
+  imports: [RouterOutlet],
+  template: `<router-outlet />`,
 })
 export class AppComponent {
 }
